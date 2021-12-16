ddb2bf519ca6e82574d7e24a9264eb5b5c27c603
Rustam <atabievrustam@mail.ru>
2021-12-16 21:51:57 +0300

    tips

--- dist/js/script.js
+++ dist/js/script.js
@@ -232,6 +232,42 @@ if(document.querySelector('.slider-rooms__body')){
 	}); 
 }
 
+if(document.querySelector('.slider-tips__body')){
+	new Swiper('.slider-tips__body', {
+		observer: true,
+		observeParents: true,
+		slidesPerView: '3',
+		spaceBetween: 32,
+		speed: 800,
+		loop: true,
+		watchOverflow: true,
+		// Dotts 
+		pagination: {
+			el: '.slider-tips__dotts',
+			clickable: true,
+		},
+		//Arrows
+		navigation: {
+			nextEl: '.slider-tips .slider-arrow_next',
+			prevEl: '.slider-tips .slider-arrow_prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1.1,
+				spaceBetween: 15
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 32
+			}
+		}
+	}); 
+}
+
 /*
 new Swiper('.swiper-container',{
 	//Стрелки
